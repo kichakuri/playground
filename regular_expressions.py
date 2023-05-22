060c81cf078c74b0ba5bee6b0f2c0cfa8ba4a86a
benard@gmail.com <benard@gmail.com>
2023-05-22 11:35:58 +0300

    Trying something out with reading from CSV files

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,4 +1,6 @@
 import re
+import csv
+import
 
 
 def get_first_regex():
@@ -30,3 +32,14 @@ def get_first_regex():
 #        print(match.start(), match.end())
 #        print(match.span)
 #        print(match.group()[0])
+
+def read_from_CSV(sourcefile:str):
+    sourcefile="C:\Users\benar\OneDrive\Documents\Litemore Ltd\Admin and Finance\Fin Reporting\2023\Reports\RevenueLeakageAnalytics\RawData\school proforma invoice  dates and exam count"
+    with open(sourcefile) as data_file:
+        reader=csv.reader(data_file)
+        next(reader,none)
+        data = []
+        for row in reader:
+            mydict={"School":row[0],"Code":row[1],"ValidityStatus":row[2],"CreationDate":row[3],"Country":row[4],"BoardingStatus":row[9],"Ownership":row[11],"SchoolGender":row[12],"ProformaAmt":row[13],"ProformaDate":row[14],"InvoiceAmt",row[15],"InvoiceDate",row[16],"ExamsProcessed":row[17]}
+            data.append(mydict)
+        return data
