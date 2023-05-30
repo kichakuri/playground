e0b1b5bb461405f5b1b1b06daeb46244495bb7b5
benard@gmail.com <benard@gmail.com>
2023-05-30 18:34:58 +0300

    Working on computed month field in pandas practice

--- pandas_practice.py
+++ pandas_practice.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 def get_monthly_revenue():
     raw_data = pd.read_csv('schoolproformainvoicedatesandexamcount.csv',
-                           index_col='Registration',
                            usecols=['School Name', 'Creation Date', 'Proforma Amount', 'Proforma Date',
                                     'Invoice Amount', 'Invoice Date', 'Exam Series Created'])
-    return raw_data
+    invoice_date = pd.to_datetime(raw_data['Invoice Date'])
+    raw_data['Invoice Month']=invoice_date.month
+    raw_data['Invoice Year']=invoice_date.year
+    return raw_data['Invoice Month']
