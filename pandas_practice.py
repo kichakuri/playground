700e6376a6bdf271e2f0b3c8f59eb32a078fda24
benard@gmail.com <benard@gmail.com>
2023-05-30 19:53:41 +0300

    Able to aggregate by month

--- pandas_practice.py
+++ pandas_practice.py
@@ -5,7 +5,14 @@ def get_monthly_revenue():
     raw_data = pd.read_csv('schoolproformainvoicedatesandexamcount.csv',
                            usecols=['School Name', 'Creation Date', 'Proforma Amount', 'Proforma Date',
                                     'Invoice Amount', 'Invoice Date', 'Exam Series Created'])
-    invoice_date = pd.to_datetime(raw_data['Invoice Date'])
-    raw_data['Invoice Month']=invoice_date.month
-    raw_data['Invoice Year']=invoice_date.year
-    return raw_data['Invoice Month']
+    date_column = pd.DatetimeIndex(raw_data['Invoice Date'], dayfirst=True)
+    raw_data['Month'] = date_column.month
+    pd.set_option('display.max_columns', 8)
+    raw_data = raw_data.groupby('Month')['Invoice Amount'].sum()
+    return raw_data
+
+
+if __name__ == '__main__':
+    # datafile = get_raw_data()
+    print(get_monthly_revenue())
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
