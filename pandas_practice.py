b84e2e9b7e11c8f91a7fed0ba91a0815162ea8b3
kichakuri <benardkichakuri@gmail.com>
2023-06-01 21:49:00 +0300

    Saving Thursdays work

--- pandas_practice.py
+++ pandas_practice.py
@@ -10,15 +10,10 @@ def get_monthly_revenue():
     # raw_data['Invoice Date'].replace(to_replace=lambda x: 0 if x == 'NULL' else x, inplace=True)
     # raw_data['Invoice Amount'].replace(to_replace=lambda x: 0 if x == 'NULL' else x, inplace=True)
     pd.set_option('display.max_columns', 8)
-    raw_data = raw_data.groupby('Month')['Invoice Amount'].sum()
-    return raw_data
-
-
-def get_total_invoice_amounts():
-    rawdata = get_monthly_revenue().raw_data
-    total_invoice_amounts = rawdata['Invoice Amount'].sum()
-    return total_invoice_amounts
-
+    sales_by_month = raw_data.groupby('Month').agg({'Proforma Amount':sum,'Invoice Amount':sum})
+    print(sales_by_month)
+    print(f'Total Invoice Amount: ',raw_data['Invoice Amount'].sum())
+    return
 
 if __name__ == '__main__':
     # datafile = get_raw_data()
